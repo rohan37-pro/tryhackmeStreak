569e2f816cf9278fc3cf14ddb49fbd9aaaf85bb7
rohan37-pro <rohan2002maji@gmail.com>
2024-02-09 16:21:13 +0530

    basic structure

---
+++ main.py
@@ -0,0 +1,30 @@
+import time
+import utils
+import json
+import platform
+
+
+### loading user configs ###
+with open("./configs/userConfig.json", 'r') as file:
+    users = json.load(file)
+
+if platform.system() == "Windows":
+    utils.auto_connect_wifi()
+
+
+
+for usr in users:
+    if utils.check_today(users[usr]["date"]):
+        quit()
+    driver = utils.get_driver(usr)
+
+    if users[usr]["login"] == False:
+        utils.login(driver)
+        users[usr]["login"] == True
+        with open("./configs/userConfig.json", 'w') as file:
+            json.dump(users, file)
+    
+
+        
+    
+
---
+++ utils.py
@@ -0,0 +1,82 @@
+import time
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
+from tqdm import tqdm
+import pyperclip
+import platform
+from datetime import datetime
+import subprocess
+
+
+def get_driver(profile):
+    if platform.system() == "Linux":
+        option = webdriver.ChromeOptions()
+        option.add_argument(f"--user-data-dir=./user-data-dir/{profile}")
+        driver = webdriver.Chrome(options=option)
+
+
+def wait(t):
+    for i in tqdm(range(t)):
+        time.sleep(1)
+
+def login(driver) :
+    driver.get("https://tryhackme.com/login")
+    wait(60)
+
+
+
+def check_today(d):
+    t = datetime.timetuple(datetime.today())
+    timef = str(t.tm_mday) + "/" + str(t.tm_mon) + "/" + str(t.tm_year)
+    if d == timef:
+        return True
+    else:
+        return False
+
+def check_wifi_state():
+    state = subprocess.run('netsh wlan show interfaces | findstr /r "State"', stdout=subprocess.PIPE, text=True, shell=True)
+    state = state.stdout.strip().split(":")[1].strip()
+    if state=='connected':
+        return True
+    else:
+        return False
+
+def available_wifi( profiles ):
+    available = subprocess.run('netsh wlan show networks | findstr "SSID"', stdout=subprocess.PIPE, text=True, shell=True)
+    available = available.stdout.strip().split('\n')
+    av = []
+    for i in available:
+        av.append(i.split(':')[1].strip())
+
+    available= []
+    for i in av:
+        if i in profiles:
+            available.append(i)
+    return available
+
+
+def auto_connect_wifi():
+    ## check if already connected
+    'netsh wlan show networks | findstr /I "ssid"'
+    profile = subprocess.run('netsh wlan show profiles | findstr ":"', stdout=subprocess.PIPE, text=True, shell=True)
+    profile = profile.stdout.strip().split("\n")[1:]
+    profiles = [ i.split(':')[1].strip() for i in profile]
+
+    
+    while check_wifi_state()==False:
+        list_wifi = available_wifi(profiles)
+        if list_wifi==[]:
+            print("no wifi available...")
+            wait(30)
+            print("rechecking...")
+        else:
+            for wifi in list_wifi:
+                con = subprocess.run(f'netsh wlan connect ssid="{wifi}" name="{wifi}" interface="Wi-Fi"', stdout=subprocess.PIPE, text=True, shell=True)
+                print(f"trying to connect to {wifi}")
+                wait(10)
+                if check_wifi_state()==False:
+                    print("connection failed..")
+                    continue
+                else:
+                    break
